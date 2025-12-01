55f13cd61d571c3b74c2443153d1372ce92d6ae2
werapi <sokneip@tuta.io>
2025-12-01 11:25:24 +0100

    feat: add tablet_rotation to config options

--- src/config/parse_config.h
+++ src/config/parse_config.h
@@ -286,6 +286,8 @@ typedef struct {
 
 	double axis_scroll_factor;
 
+	int32_t tablet_rotation;
+
 	int32_t blur;
 	int32_t blur_layer;
 	int32_t blur_optimized;
@@ -1679,6 +1681,8 @@ bool parse_option(Config *config, char *key, char *value) {
 		config->button_map = atoi(value);
 	} else if (strcmp(key, "axis_scroll_factor") == 0) {
 		config->axis_scroll_factor = atof(value);
+	} else if (strcmp(key, "tablet_rotation") == 0) {
+		config->tablet_rotation = CLAMP_INT(atoi(value), 0, 7);
 	} else if (strcmp(key, "gappih") == 0) {
 		config->gappih = atoi(value);
 	} else if (strcmp(key, "gappiv") == 0) {
@@ -3233,6 +3237,7 @@ void override_config(void) {
 	config.button_map = CLAMP_INT(config.button_map, 0, 1);
 	config.axis_scroll_factor =
 		CLAMP_FLOAT(config.axis_scroll_factor, 0.1f, 10.0f);
+	config.tablet_rotation = CLAMP_INT(config.tablet_rotation, 0, 7);
 	config.gappih = CLAMP_INT(config.gappih, 0, 1000);
 	config.gappiv = CLAMP_INT(config.gappiv, 0, 1000);
 	config.gappoh = CLAMP_INT(config.gappoh, 0, 1000);
@@ -3325,6 +3330,7 @@ void set_value_default() {
 	config.scratchpad_cross_monitor = 0;
 	config.focus_cross_tag = 0;
 	config.axis_scroll_factor = 1.0;
+  config.tablet_rotation = 0;
 	config.view_current_to_back = 0;
 	config.single_scratchpad = 1;
 	config.xwayland_persistence = 1;
